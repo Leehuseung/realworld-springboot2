31f3244c29fae83ae2482a02a411d307f74a5906
Leehuseung <gntmd4261@naver.com>
2022-07-20 10:30:14 +0900

    first commit

---
+++ src/main/java/com/kr/realworldspringboot2/RealworldSpringboot2Application.java
@@ -0,0 +1,13 @@
+package com.kr.realworldspringboot2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealworldSpringboot2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealworldSpringboot2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kr/realworldspringboot2/RealworldSpringboot2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kr.realworldspringboot2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealworldSpringboot2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
