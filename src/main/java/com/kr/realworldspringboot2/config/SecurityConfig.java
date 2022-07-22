a47122a64505dec7f651173ffc9bc20e544e259a
Leehuseung <gntmd4261@naver.com>
2022-07-22 10:03:31 +0900

    InMemoryUserDetailsManager에서 직접 구현한 UserDetailsService를 구현
    - 멤버권한은 USER,MANAGER,ADMIN 구현
    - user / 1111을 이용해 로그인 가능

--- src/main/java/com/kr/realworldspringboot2/config/SecurityConfig.java
+++ src/main/java/com/kr/realworldspringboot2/config/SecurityConfig.java
@@ -1,13 +1,13 @@
 package com.kr.realworldspringboot2.config;
 
 
+import com.kr.realworldspringboot2.member.AuthUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -19,25 +19,26 @@ public class SecurityConfig {
     }
 
     @Bean
-    public UserDetailsService userDetailsService () {
-        return new InMemoryUserDetailsManager();
-    };
+    public UserDetailsService userDetailsService(){
+        return new AuthUserDetailsService();
+    }
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
         authenticationManagerBuilder
-                .userDetailsService(userDetailsService()).passwordEncoder(passwordEncoder())
-                .and()
-                .inMemoryAuthentication().withUser("user1")
-                .password("$2a$10$0W1VWLXgCZXsMUHbDXoJj.NiQgKl21UaZVHs331eArkI7UfHcjIaO") //1111
-                .roles("USER");
+                .userDetailsService(userDetailsService())
+                .passwordEncoder(passwordEncoder());
 
         http
             .csrf().disable()
             .formLogin().permitAll()
             .and()
             .authorizeRequests()
+            .antMatchers("/all").permitAll()
+            .antMatchers("/user").hasRole("USER")
+            .antMatchers("/manager").hasRole("MANAGER")
+            .antMatchers("/admin").hasRole("ADMIN")
             .anyRequest().authenticated()
             .and()
             .authenticationManager(authenticationManagerBuilder.build());
---
+++ src/main/java/com/kr/realworldspringboot2/member/MemberRole.java
@@ -0,0 +1,5 @@
+package com.kr.realworldspringboot2.member;
+
+public enum MemberRole {
+    USER, MANAGER, ADMIN
+}
