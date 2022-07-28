9fd8be8eb745c37d77465361bdbfad18cfe88d42
Leehuseung <gntmd4261@naver.com>
2022-07-28 14:32:09 +0900

    CORS 설정 추가

--- src/main/java/com/kr/realworldspringboot2/config/SecurityConfig.java
+++ src/main/java/com/kr/realworldspringboot2/config/SecurityConfig.java
@@ -6,6 +6,7 @@ import com.kr.realworldspringboot2.security.ApiLoginFilter;
 import com.kr.realworldspringboot2.security.AuthUserDetailsService;
 import com.kr.realworldspringboot2.util.JWTUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
@@ -15,9 +16,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 public class SecurityConfig {
+    @Value("${real-world.allowed-Origin}")
+    String allowedOrigin;
 
     @Bean
     public BCryptPasswordEncoder passwordEncoder() {
@@ -33,6 +39,18 @@ public class SecurityConfig {
     }
 
     @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.addAllowedOrigin(allowedOrigin);
+        configuration.addAllowedMethod("*");
+        configuration.addAllowedHeader("*");
+        configuration.setAllowCredentials(true);
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         //AuthenticationManager설정
         AuthenticationManagerBuilder authenticationManagerBuilder = http.getSharedObject(AuthenticationManagerBuilder.class);
@@ -41,6 +59,8 @@ public class SecurityConfig {
 
         http
             .csrf().disable()
+            .cors().configurationSource(corsConfigurationSource())
+            .and()
             .authorizeRequests()
             .antMatchers("/all").permitAll()
             .antMatchers("/user").hasRole("USER")
