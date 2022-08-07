e9e0534a72c5a25014a474f7f7f196102f29bdf7
Leehuseung <gntmd4261@naver.com>
2022-08-08 08:58:35 +0900

    Security 관련 테스트 개선
    - data.sql 사용하도록 변경
    - body String에서 json객체 사용하도록 변경

--- src/test/java/com/kr/realworldspringboot2/config/SecurityConfigTest.java
+++ src/test/java/com/kr/realworldspringboot2/config/SecurityConfigTest.java
@@ -1,8 +1,7 @@
 package com.kr.realworldspringboot2.config;
 
-import com.kr.realworldspringboot2.member.Member;
 import com.kr.realworldspringboot2.member.MemberRepository;
-import org.junit.jupiter.api.BeforeEach;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -18,6 +18,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 class SecurityConfigTest {
 
     @Autowired
@@ -25,28 +26,16 @@ class SecurityConfigTest {
     @Autowired
     PasswordEncoder passwordEncoder;
 
-    @BeforeEach
-    public void default_delete() {
-        memberRepository.deleteAll();
-    }
-
     @Test
     @DisplayName("로그인 성공 테스트")
     public void login_success_test(@Autowired MockMvc mvc) throws Exception {
-        //given
-        Member test01 = Member.builder()
-                .email("test01@realworld.com")
-                .username("test01")
-                .password(passwordEncoder.encode("1111"))
-                .build();
-        memberRepository.save(test01);
+        JSONObject body = new JSONObject();
+        JSONObject user = new JSONObject();
+        user.put("email","test01@realworld.com");
+        user.put("password","1111");
+        body.put("user",user);
 
-
-        String body = "{\"user\":{\"email\":\"test01@realworld.com\",\"password\":\"1111\"}}";
-
-        //when
-        mvc.perform(post("/api/users/login").contentType(MediaType.APPLICATION_JSON).content(body))
-                //then
+        mvc.perform(post("/api/users/login").contentType(MediaType.APPLICATION_JSON).content(body.toString()))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.user.email").value("test01@realworld.com"))
                 .andExpect(jsonPath("$.user.username").value("test01"))
@@ -56,19 +45,13 @@ class SecurityConfigTest {
     @Test
     @DisplayName("로그인 실패 테스트")
     public void login_fail_test(@Autowired MockMvc mvc) throws Exception {
-        //given
-        Member test01 = Member.builder()
-                .email("test01@realworld.com")
-                .username("test01")
-                .password(passwordEncoder.encode("1111"))
-                .build();
-        memberRepository.save(test01);
+        JSONObject body = new JSONObject();
+        JSONObject user = new JSONObject();
+        user.put("email","noId@realworld.com");
+        user.put("password","1111");
+        body.put("user",user);
 
-        //when
-        String body = "{\"user\":{\"email\":\"noId@realworld.com\",\"password\":\"1111\"}}";
-
-        //then
-        mvc.perform(post("/api/users/login").contentType(MediaType.APPLICATION_JSON).content(body))
+        mvc.perform(post("/api/users/login").contentType(MediaType.APPLICATION_JSON).content(body.toString()))
                 .andExpect(status().isUnauthorized());
     }
 
@@ -76,7 +59,6 @@ class SecurityConfigTest {
     @Test
     @DisplayName("사용자 헤더 존재 안할 때 테스트")
     public void none_token_header(@Autowired MockMvc mvc) throws Exception {
-        //then
         mvc.perform(post("/api/users/login"))
                 .andExpect(status().isUnauthorized());
     }
@@ -84,11 +66,13 @@ class SecurityConfigTest {
     @Test
     @DisplayName("사용자 이메일 존재 안할 때 테스트")
     public void none_email_header(@Autowired MockMvc mvc) throws Exception {
-        //when
-        String body = "{\"user\":{\"email\":,\"password\":\"1111\"}}";
+        JSONObject body = new JSONObject();
+        JSONObject user = new JSONObject();
+        user.put("email",null);
+        user.put("password","1111");
+        body.put("user",user);
 
-        //then
-        mvc.perform(post("/api/users/login").contentType(MediaType.APPLICATION_JSON).content(body))
+        mvc.perform(post("/api/users/login").contentType(MediaType.APPLICATION_JSON).content(body.toString()))
                 .andExpect(status().isUnauthorized());
     }
 
