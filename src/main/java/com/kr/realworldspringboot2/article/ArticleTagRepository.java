0a9cc3fb74ca333092d5bb98382f3518ae3499d5
Leehuseung <gntmd4261@naver.com>
2022-08-18 11:07:41 +0900

    ArticleTag remove 오류 수정

--- src/main/java/com/kr/realworldspringboot2/article/ArticleTagRepository.java
+++ src/main/java/com/kr/realworldspringboot2/article/ArticleTagRepository.java
@@ -1,9 +1,13 @@
 package com.kr.realworldspringboot2.article;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ArticleTagRepository extends JpaRepository<ArticleTag, Long> {
 
+    @Transactional
+    @Modifying
     void deleteArticleTagsByArticle(Article article);
 
 }
