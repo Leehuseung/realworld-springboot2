5b5489f976dde4c8428a6afdd2295e63cf26d6ae
Leehuseung <gntmd4261@naver.com>
2022-08-18 11:47:51 +0900

    Member entity 사용 안하는 함수 제거

--- src/main/java/com/kr/realworldspringboot2/member/Member.java
+++ src/main/java/com/kr/realworldspringboot2/member/Member.java
@@ -1,10 +1,7 @@
 package com.kr.realworldspringboot2.member;
 
 
-import com.kr.realworldspringboot2.article.Article;
-import com.kr.realworldspringboot2.article.favorite.ArticleFavorite;
 import com.kr.realworldspringboot2.profile.Follow;
-import com.kr.realworldspringboot2.profile.ProfileDTO;
 import com.sun.istack.NotNull;
 import lombok.*;
 import org.springframework.lang.Nullable;
@@ -45,10 +42,6 @@ public class Member extends BaseEntity{
     @Enumerated(EnumType.STRING)
     private List<MemberRole> roles = new ArrayList<>();
 
-    @OneToMany(mappedBy = "member")
-    @Builder.Default
-    private Set<Follow> follows = new HashSet<>();
-
     @OneToMany(mappedBy = "followMember")
     @Builder.Default
     private Set<Follow> followee = new HashSet<>();
@@ -62,24 +55,6 @@ public class Member extends BaseEntity{
         return followee.contains(follow);
     }
 
-    public boolean isFavoriteArticle(Article article) {
-        ArticleFavorite articleFavorite = ArticleFavorite.builder()
-                .article(article)
-                .member(this)
-                .build();
-
-        return article.getArticleFavorites().contains(articleFavorite);
-    }
-
-    public ProfileDTO toProfileDTO(Member loginMember){
-        return ProfileDTO.builder()
-                .username(this.username)
-                .bio(this.bio)
-                .image(this.image)
-                .following(isFollowee(loginMember))
-                .build();
-    }
-
     public AuthorDTO toAuthorDTO(Member loginMember){
         return AuthorDTO.builder()
                 .username(this.username)
@@ -89,8 +64,4 @@ public class Member extends BaseEntity{
                 .build();
     }
 
-
-    public void addMemberRole(MemberRole memberRole) {
-        roles.add(memberRole);
-    }
 }
