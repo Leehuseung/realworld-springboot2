fe7882ccba38ef3a103c5384c25699e170b03dd5
Leehuseung <gntmd4261@naver.com>
2022-08-18 11:07:56 +0900

    UnFavorite Article remove 오류 수정

--- src/main/java/com/kr/realworldspringboot2/article/favorite/ArticleFavoriteRepository.java
+++ src/main/java/com/kr/realworldspringboot2/article/favorite/ArticleFavoriteRepository.java
@@ -3,6 +3,8 @@ package com.kr.realworldspringboot2.article.favorite;
 import com.kr.realworldspringboot2.article.Article;
 import com.kr.realworldspringboot2.member.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 public interface ArticleFavoriteRepository extends JpaRepository<ArticleFavorite,Long> {
 
@@ -10,6 +12,8 @@ public interface ArticleFavoriteRepository extends JpaRepository<ArticleFavorite
 
     void deleteArticleFavoritesByArticle(Article article);
 
+    @Transactional
+    @Modifying
     void deleteArticleFavoriteByArticleAndMember(Article article, Member member);
 
 }
